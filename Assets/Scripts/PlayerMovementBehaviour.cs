1189e378cdfc4e86627abd4a40780bfc477b7e61
Jeremy Barzas <jbarzas@gmail.com>
2017-07-05 13:03:05 -0500

    added test scene
    added test scene and character movement script

---
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    /*
+    private float previousY;
+    private bool jumping = false;
+    */         
+	
+	void Update ()
+    {
+        PlayerMove();
+	}
+
+    void PlayerMove()
+    {
+        // forward, backward, stafe
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10f;        
+        transform.Translate(x, 0, z);
+
+        // mouse turn
+        var turn = Input.GetAxis("Mouse X") * Time.deltaTime * 150f;
+        transform.Rotate(0, turn, 0);       
+    }
+}
+
+/*
+       // jump
+       if (jumping == false)
+       {
+           jumping = true;
+           if (Input.GetKeyDown(KeyCode.Space))
+           {
+               previousY = transform.position.y;
+           }
+       }
+
+       if (Input.GetKey(KeyCode.Space))
+       {
+           previousY = transform.position.y;
+           var jumpforce = previousY + Time.deltaTime;
+           transform.Translate(0, jumpforce, 0);
+       }
+       else
+       {
+           jumping = false;
+       }
+       */
