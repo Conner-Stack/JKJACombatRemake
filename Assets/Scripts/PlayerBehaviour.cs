7c805c334124727c16c703695e532f38dd074201
Jeremy Barzas <jbarzas@gmail.com>
2017-07-05 15:16:16 -0500

    syncing work

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Player playerConfig;
+    public Player playerRuntime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerRuntime = playerConfig;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {		
+
+	}
+}
